c1f92262aab4f176bf5e9b56884c934a63327432
amanshanti <90171291+amanshanti@users.noreply.github.com>
2022-05-26 22:51:35 +0530

    Create Sort012.cpp

---
+++ Sort012.cpp
@@ -0,0 +1,22 @@
+void sort012(int a[], int n)
+{
+        int count0 = 0;
+        int count1 = 0;
+        for(int i=0;i<n;i++){
+            if(a[i]==0){
+                count0++;
+            }
+            else if(a[i]==1){
+                count1++;
+            }
+        }
+        for(int i=0;i<count0;i++){
+            a[i]=0;
+        }
+        for(int i=count0;i<count0+count1;i++){
+            a[i]=1;
+        }
+        for(int i=count0+count1;i<n;i++){
+            a[i]=2;
+        }
+    }
